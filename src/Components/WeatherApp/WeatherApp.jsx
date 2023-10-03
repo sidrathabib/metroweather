f36fd7b291aebd77b0199d04abe19d9d908eb4e6
Sidrat Habib <sidrathabib@gmail.com>
2023-10-03 19:37:44 -0400

    mostly done. debugging

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState} from 'react';
 import './WeatherApp.css'
 
 import search_icon from '../Assets/search.png'
@@ -11,19 +11,51 @@ import wind from '../Assets/wind.png'
 import humidity from '../Assets/humidity.png'
 
 
-
-
 export const WeatherApp = () => {
   
   let api_key ="46dd8c6a1b5d341de5815963f8c14496";
-  const search = () => {
+  
+  const[wicon, setWicon] = useState(cloud);
+
+  const search = async () => {
     const element = document.getElementsByClassName("cityInput");
     if(element[0].value==="") {
       return 0;
     }
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temperature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location")
+    
+    humidity[0].innerHTML = data.main.humidity + " %";
+    wind[0].innerHTML = Math.floor(data.wind.speed) + " km/h";
+    temperature[0].innerHTML = Math.floor(data.main.temp) + " °C";
+    location[0].innerHTML = data.name;
+  
+    if(data.weather[0].icon==="01d" || data.weather[0].icon==="01n"){
+      setWicon(clear);
+    } else if (data.weather[0].icon==="02d" || data.weather[0].icon==="02n"){
+      setWicon(cloud);
+    } else if (data.weather[0].icon==="03d" || data.weather[0].icon==="03n"){
+      setWicon(drizzle);
+    } else if (data.weather[0].icon==="04d" || data.weather[0].icon==="04n"){
+      setWicon(drizzle);
+    } else if (data.weather[0].icon==="09d" || data.weather[0].icon==="09n"){
+      setWicon(rain);
+    } else if (data.weather[0].icon==="10d" || data.weather[0].icon==="10n"){
+      setWicon(rain);
+    } else if (data.weather[0].icon==="13d" || data.weather[0].icon==="13n"){
+      setWicon(snow);
+    } else {
+      setWicon(clear);
+    }
+
   }
   
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=London&units=Metric&appid=${api_key}`;
   
   return (
     <div className='container'>
@@ -52,7 +84,7 @@ export const WeatherApp = () => {
         <div className="element">
           <img src={wind} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percent">18 km/h</div>
+            <div className="wind-rate">18 km/h</div>
             <div className="text">Wind Speed</div>
           </div>
         </div>
